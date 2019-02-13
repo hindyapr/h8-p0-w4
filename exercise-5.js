e6cc9ac8c8719d27779798a82a184c2c867586dc
hindyapr <hindyapr@gmail.com>
2019-02-13 13:21:56 +0700

    exercise-5 edited

--- exercise-5.js
+++ exercise-5.js
@@ -7,8 +7,11 @@ function ubahHuruf(kata) {
     var tempChar = '';
 
     for (var i = 0; i < kata.length; i++) {
-        tempChar = String.fromCharCode(kata[i].charCodeAt(0) + 1);
-        lbub = lbub + tempChar;
+        if (kata[i] === 'z') lbub = lbub + 'a';
+        else {
+            tempChar = String.fromCharCode(kata[i].charCodeAt(0) + 1);
+            lbub = lbub + tempChar;
+        }
     }
 
     return lbub;
@@ -20,3 +23,4 @@ console.log(ubahHuruf('developer')); // efwfmpqfs
 console.log(ubahHuruf('javascript')); // kbwbtdsjqu
 console.log(ubahHuruf('keren')); // lfsfo
 console.log(ubahHuruf('semangat')); // tfnbohbu
+console.log(ubahHuruf('z'));
